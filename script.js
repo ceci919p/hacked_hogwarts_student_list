2ff05793baa6328c524232d3460598d89d6d852d
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2022-02-23 18:36:21 +0100

    middlename not working yet

--- script.js
+++ script.js
@@ -31,20 +31,109 @@ async function loadJSON() {
   const jsonData = await fetch(url);
   hogwartsData = await jsonData.json();
 
-  //show hogwarts data in a table in the console (for testing)
-  console.table(hogwartsData);
-
   prepareStudents();
 }
 
 //delegator to other functions
-function prepareStudents() {}
+function prepareStudents() {
+  hogwartsData.forEach((stud) => {
+    const student = Object.create(Student);
 
-function getFirstName() {}
-function getMiddleName() {}
-function getNickName() {}
+    /*     const fullMiddleName = fullname.substring(
+      fullname.indexOf(" ") + 1,
+      fullname.lastIndexOf(" ")
+    ); */
+
+    student.firstName = getFirstName(stud.fullname.trim());
+    student.middleName = getMiddleName(stud.fullname.trim());
+    student.nickName = getNickName(stud.fullname.trim());
+    student.lastName = getLastName(stud.fullname.trim());
+
+    //put student in the allStudents array
+    allStudents.push(student);
+  });
+}
+
+function getFirstName(fullname) {
+  if (fullname.includes(" ")) {
+    let firstName = fullname.substring(
+      fullname.indexOf(0),
+      fullname.indexOf(" ")
+    );
+
+    const cleanedFirstName = cleanData(firstName);
+    return cleanedFirstName;
+  } else {
+    const cleanedFirstName = cleanData(fullname);
+    return cleanedFirstName;
+  }
+}
+/* 
+function getMiddleName(fullname) {
+  if (!fullname.includes('"')) {
+    let middleName = fullname.substring(
+      fullname.indexOf(" ") + 1,
+      fullname.lastIndexOf(" ")
+    );
+
+    const cleanedMiddleName = cleanData(middleName);
+    return cleanedMiddleName;
+  } else if (!fullname.includes("")) {
+    let middleName = null; 
+    return middleName;  //this doesn't work
+  }
+} */
+
+/* function getMiddleName(fullname) {
+  const fullMiddleName = fullname
+    .substring(fullname.indexOf(" ") + 1, fullname.lastIndexOf(" "))
+    .trim();
+
+  if (!fullname.includes('"')) {
+    let middleName = fullMiddleName;
+
+    const cleanedMiddleName = cleanData(middleName);
+    return cleanedMiddleName;
+  }
+} */
+
+function getMiddleName(fullname) {
+  const firstSpace = fullname.indexOf(" ");
+  const lastSpace = fullname.lastIndexOf(" ");
+  const fullMiddleName = fullname.substring(firstSpace + 1, lastSpace).trim();
+  if (!fullname.includes('"') && fullname.includes(" ")) {
+    let middleName =
+      fullMiddleName.substring(0, 1) + fullMiddleName.substring(1);
+    const cleanedMiddleName = cleanData(middleName);
+    return cleanedMiddleName;
+  } else if (fullMiddleName === "") {
+    let middleName = null;
+    return middleName;
+  }
+}
+
+function getNickName(fullname) {
+  //check if middlename contains "" if that is true, it is a nickName
+
+  if (fullname.includes('"')) {
+    let nickName = fullname.substring(
+      fullname.indexOf('"') + 1,
+      fullname.lastIndexOf('"')
+    );
+
+    const cleanedNickName = cleanData(nickName);
+    return cleanedNickName;
+  }
+}
+
 function getLastName() {}
 function getGender() {}
 function getHouse() {}
 function getProfilePic() {}
-function cleanData() {}
+
+function cleanData(data) {
+  const capitalizedFirstLetter = data.slice(0, 1).toUpperCase();
+  const lowerCaseTheRest = data.slice(1).toLowerCase();
+  const cleanedData = capitalizedFirstLetter + lowerCaseTheRest;
+  return cleanedData;
+}
